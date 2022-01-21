6d022de99b97c8ec6ce8e04ce9fa5c134977a377
Baldonak <victormaynougomez@gmail.com>
2022-01-21 17:07:47 +0000

    Alpha format version

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+const suits = ["♥", "♦", "♠", "♣"];
+const numbers = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const top_icon_container = document.querySelector(".icon_container.top");
+const bottom_icon_container = document.querySelector(".icon_container.bottom");
+
+const random_item = items => {
+  return items[Math.floor(Math.random() * items.length)];
+};
+
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let random_icon = random_item(suits);
+  top_icon_container.innerHTML = random_icon;
+  bottom_icon_container.innerHTML = random_icon;
 };
