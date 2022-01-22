0cbb9759b1ba7b42588d852081b822f8c0d61613
Baldonak <victormaynougomez@gmail.com>
2022-01-22 14:01:40 +0000

    Alpha version

--- src/app.js
+++ src/app.js
@@ -5,32 +5,64 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const suits = ["♥", "♦", "♠", "♣"];
-const numbers = [
-  "A",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "J",
-  "Q",
-  "K"
-];
-const top_icon_container = document.querySelector(".icon_container.top");
-const bottom_icon_container = document.querySelector(".icon_container.bottom");
+function Card() {
+  this.suits = ["♥", "♦", "♠", "♣"];
+  this.numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+}
 
-const random_item = items => {
-  return items[Math.floor(Math.random() * items.length)];
+Card.prototype = {
+  //Devuelve un elemento aleatorio de una matriz
+  RandomItem: function(items) {
+    return items[Math.floor(Math.random() * items.length)];
+  },
+
+  //Inserta un icono en un elemento
+  InsertIconIntoElement: function(icon, element) {
+    element.innerHTML = icon;
+  },
+  //Devuelve "rojo" si suit es corazon o pica, y negro si no lo es
+  ColorSuitFont: function(random_suit) {
+    return random_suit === "♥" || random_suit === "♦" ? "red" : "black";
+  },
+  //Cambia color
+  ChangeColorSuitFont: function(color_suit, element) {
+    document.querySelector(element).style.color = color_suit;
+  },
+  //Renderiza una carta aleatoria
+  getRandomCard: function() {
+    let random_suit = this.RandomItem(this.suits);
+    let random_number = this.RandomItem(this.numbers);
+    let color = this.ColorSuitFont(random_suit);
+
+    this.ChangeColorSuitFont(color, "body");
+
+    const top_icon_container = document.querySelector(".icon_container.top");
+    const bottom_icon_container = document.querySelector(
+      ".icon_container.bottom"
+    );
+    const number_container = document.querySelector(".number_container h1");
+
+    this.InsertIconIntoElement(random_suit, top_icon_container);
+    this.InsertIconIntoElement(random_suit, bottom_icon_container);
+    this.InsertIconIntoElement(random_number, number_container);
+  }
 };
 
 window.onload = () => {
-  //write your code here
-  let random_icon = random_item(suits);
-  top_icon_container.innerHTML = random_icon;
-  bottom_icon_container.innerHTML = random_icon;
+  Card = new Card();
+  Card.getRandomCard();
 };
